9d051e4a7e9c19abb3f9ceb9fe994af75dc6b89f
Harsha Vardhan <94213293+harsha42703@users.noreply.github.com>
2023-08-29 15:49:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import './App.css';
 import myapi from './data';
+import React, { useState } from 'react';
+
+
+//---------------------------------
+
+
+const QuotesComponent = ({ quote }) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(quote);
+    setIsCopied(true);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 2000);
+  };
+
+  return (
+    <div className="quotes-component">
+      <p className="quote">{quote}</p>
+      <button className={`copy-button ${isCopied ? 'copied' : ''}`} onClick={handleCopyClick}>
+        {isCopied ? 'Copied!' : 'Copy'}
+      </button>
+    </div>
+  );
+};
 
 
 
+
+//---------------------------------
 
 const Box = (props) => {
   const {quote,author} = props;
@@ -10,6 +39,7 @@ const Box = (props) => {
     <section className='body'>
       <h1 className='quote'>{quote}</h1>
       <h6 className='author'>{author}</h6>
+    </QuotesComponent>
     </section>
 
   );
