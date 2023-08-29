cb05e8e09308be12070bf62b74e88619c8de255b
Harsha Vardhan <94213293+harsha42703@users.noreply.github.com>
2023-08-29 16:31:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,65 +1,46 @@
 import './App.css';
 import myapi from './data';
-import React, { useState } from 'react';
+import React,{useState} from 'react';
 
-
-//---------------------------------
-
-
-const QuotesComponent = ({ quote }) => {
-  const [isCopied, setIsCopied] = useState(false);
+const Box = (props) => {
+  const { quote, author } = props;
+  const [copy, setCopy] = useState(false);
 
   const handleCopyClick = () => {
+    setCopy(true);
     navigator.clipboard.writeText(quote);
-    setIsCopied(true);
-
-    setTimeout(() => {
-      setIsCopied(false);
-    }, 2000);
   };
 
   return (
-    <div className="quotes-component">
-      <p className="quote">{quote}</p>
-      <button className={`copy-button ${isCopied ? 'copied' : ''}`} onClick={handleCopyClick}>
-        {isCopied ? 'Copied!' : 'Copy'}
-      </button>
-    </div>
-  );
-};
-
-
-
-
-//---------------------------------
-
-const Box = (props) => {
-  const {quote,author} = props;
-  return(
     <section className='body'>
       <h1 className='quote'>{quote}</h1>
-      <h6 className='author'>{author}</h6>
-    </QuotesComponent>
+      <h6 className='author'>-{author}</h6>
+      <button
+        className='cpy-btn'
+        onClick={handleCopyClick}
+      >
+        {copy ? " Copied! " : "Copy"}
+      </button>
     </section>
-
   );
-}
-
+};
 
 function App() {
   return (
     <div className='main'>
-    <div className="App">
-      <header className='head'><h1>quote picker</h1></header>
+      <div className='App'>
+        <header className='head'>
+          <h1>Quote picker</h1>
+        </header>
         {myapi.map((eachObj) => {
-          const {id,quote,author} = eachObj;
-        return(
-          <div className='container'>
-          <Box id={id} quote={quote} author={author}/>
-          </div>
-        );
+          const { id, quote, author } = eachObj;
+          return (
+            <div className='container' key={id}>
+              <Box quote={quote} author={author} />
+            </div>
+          );
         })}
-    </div>
+      </div>
     </div>
   );
 }
